1b2aa97c9c94ff3da2068ecb20a592758dbc0987
Sylvain MARIE <sylvain.marie@se.com>
2025-05-09 17:16:59 +0200

    Removed 3.14 : pillow does not support it yet

--- noxfile.py
+++ noxfile.py
@@ -54,7 +54,7 @@ class Folders:
 
 
 ENVS = {
-    PY314: {"coverage": False, "pkg_specs": {"pip": ">19"}},
+    # PY314: {"coverage": False, "pkg_specs": {"pip": ">19"}}, error : pillow does not support it yet
     PY313: {"coverage": False, "pkg_specs": {"pip": ">19"}},
     PY312: {"coverage": False, "pkg_specs": {"pip": ">19"}},
     PY311: {"coverage": False, "pkg_specs": {"pip": ">19"}},
